bef1cdf0debcb3c21a1523b202c38f3889fe2135
robertgizelo <robertgizelo25@gmail.com>
2020-12-12 15:58:44 +0200

    Header added

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,32 @@ let path = {
     },
     src: {
         html: source_folder + "/*.html",
-        scss: source_folder + "/scss/style.scss",
+        css: source_folder + "/scss/*.scss",
         js: source_folder + "/js/script.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
         fonts: source_folder + "/fonts/*.ttf",
     },
     watch: {
         html: source_folder + "/**/*.html",
-        scss: source_folder + "/scss/**/*.scss",
+        css: source_folder + "/scss/**/*.scss",
         js: source_folder + "/js/**/*.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
     },
     clean: "./" + project_folder + "/"
 }
 
-let {src, dest} = require('gulp'),
-    gulp = require('gulp'),
-    brSync = require('browser-sync').create();
+let {src, dest} = require('gulp');
+let gulp = require('gulp');
+let brSync = require('browser-sync').create();
+let dlt = require('del');
+let scss = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
+let groupMedia = require('gulp-group-css-media-queries');
+let cleanCss = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let uglify = require('gulp-uglify-es').default;
 
-function browserSync(params) {
+function browserSync() {
     brSync.init({
         server: {
             baseDir: "./" + project_folder + "/"
@@ -45,9 +52,73 @@ function html() {
         .pipe(brSync.stream());
 }
 
-let build = gulp.series(html);
-let watch = gulp.parallel(build, browserSync);
+function css() {
+    return src(path.src.css)
+        .pipe(
+            scss({
+                outputStyle: "expanded"
+            })
+        )
+        .pipe(
+            groupMedia()
+        )
+        .pipe(
+            autoprefixer({
+                cascade: true,
+                browsers: ['last 3 versions']
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(
+            cleanCss()
+        )
+        .pipe(
+            rename({
+                extname: ".min.css"
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(brSync.stream());
+}
 
+function js() {
+    return src(path.src.js)
+        .pipe(dest(path.build.js))
+        .pipe(
+            uglify()
+        )
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(dest(path.build.js))
+        .pipe(brSync.stream());
+}
+
+function img() {
+    return src(path.src.img)
+        .pipe(dest(path.build.img))
+        .pipe(brSync.stream());
+}
+
+function watchFiles() {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], img);
+}
+
+function del() {
+    return dlt(path.clean);
+}
+
+let build = gulp.series(del, gulp.parallel(js, css, html, img));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.img = img;
+exports.js = js;
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
